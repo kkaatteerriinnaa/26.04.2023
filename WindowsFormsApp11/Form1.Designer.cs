df6b78fcb4d9eb224f735d2f12cb677197c28c33
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2023-04-23 22:19:12 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 146);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(369, 292);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(1, 39);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+
+            // Настроим ListView для отображения найденных файлов и папок
+            listView1.View = View.Details;
+            listView1.Columns.Add("Name", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Type", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Size", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Modified", -2, HorizontalAlignment.Left);
+
+            // Настроим BackgroundWorker для выполнения поиска файлов и папок в отдельном потоке
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Проверим, что выбран диск и введена маска поиска
+            if (string.IsNullOrEmpty(comboBox1.Text) || string.IsNullOrEmpty(maskedTextBox1.Text))
+            {
+                MessageBox.Show("Please select a drive and enter a search mask.");
+                return;
+            }
+
+            // Очистим список результатов поиска
+            listView1.Items.Clear();
+
+            // Запустим BackgroundWorker для выполнения поиска
+            worker.RunWorkerAsync(new SearchParams(comboBox1.Text, maskedTextBox1.Text));
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Получим параметры поиска из аргументов BackgroundWorker
+            var searchParams = (SearchParams)e.Argument;
+
+            // Выполним поиск файлов и папок
+            var foundItems = SearchFilesAndFolders(searchParams.Drive, searchParams.Mask);
+
+            // Сохраним результаты поиска в аргументах BackgroundWorker
+            e.Result = foundItems;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Получим результаты поиска из аргументов BackgroundWorker
+            var foundItems = (SearchResult)e.Result;
+
+            // Отобразим результаты поиска в ListView
+            foreach (var item in foundItems)
+            {
+                var listViewItem = new ListViewItem(item.Name);
+                listViewItem.SubItems.Add(item.IsFolder ? "Folder" : "File");
+                listViewItem.SubItems.Add(item.Size.ToString());
+                listViewItem.SubItems.Add(item.Modified.ToString());
+                listView1.Items.Add(listViewItem);
+            }
+        }
+
+        private SearchResult SearchFilesAndFolders(string driveName, string mask)
+        {
+            var foundItems = new SearchResult();
+
+                // Используем DirectoryInfo для получения списка папок и файлов
+                var rootDirectory = new DirectoryInfo(driveName);
+                var directories = rootDirectory.GetDirectories("*", SearchOption.AllDirectories);
+                var files = rootDirectory.GetFiles("*", SearchOption.AllDirectories);
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string mask = maskedTextBox1.Text;
+            string path = (string)cbDrives.SelectedItem + "\\";
+            if (!string.IsNullOrWhiteSpace(mask) && Directory.Exists(path))
+            {
+                var thread = new Thread(() => Search(mask, path));
+                thread.Start();
+            }
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(Directory.GetLogicalDrives());
+            comboBox1.SelectedIndex = 0;
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var thread in threads)
+            {
+                if (thread.IsAlive)
+                {
+                    thread.Abort();
+                }
+            }
+        }
+
+    }
+}
